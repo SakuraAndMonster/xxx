37d4668a6904a13a1fd684640f2edd94663db29b
sun <sun@qq.com>
2017-10-19 10:53:17 +0800

    修改

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -7,8 +7,14 @@ define([
     "jquery",
     //用requireJS官方提供的text查看读取模板内容
     //text插件的路径 !  模板文件的路径(不能省略.html后缀名)
-    "text!tpls/teacherListTpl.html"
-],function($,teacherListTpl){
+    "text!tpls/teacherListTpl.html",
+    //arttemplate模板引擎
+    "art",
+    //查看讲师模块
+    "teacher/show",
+    //添加讲师
+    "teacher/add"
+],function($,teacherListTpl,art,teacherShow,teacherAdd){
 
     return function(){
 
@@ -27,9 +33,25 @@ define([
             url:"/api/teacher",
             type:"get",
             success:function(res){
-                if(res.code!==200){
-                    return consoloe.log()
-                }
+                if(res.code!=200)  return consoloe.log(res.msg);
+                var result=res.result;
+                //把数据编译到模板中-->获取到真实的内容
+                var html=art.render(teacherListTpl,{
+                    r:result
+                });
+                //将事件绑定在这个新创建的特定的panel中
+                var $panel=$(html);
+                $panel.on("click","but-show",function(){
+                    //通过适当的方式获取该行对应的讲师id
+                    var tc_id=$(this).parent().attr("tc_id");
+                    teacherShow(tc_id);
+                }).on("click","btn-add",function(){
+                    //添加讲师：
+                    teacherAdd();
+
+                });
+                //把真实的内容放到页面中
+                $(".main").html($panel);
             }
         })
     }
