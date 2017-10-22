070843b12a9ed740e733d32f39b311df341287c0
sun <sun@qq.com>
2017-10-22 15:43:49 +0800

    修改

---
+++ js/category/edit.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sunxinyong on 2017/10/22.
+ */
+define([
+        "jquery",
+        "text!tpls/categoryEditTpl.html"
+    ]
+    ,function($,categoryEditTpl){
+
+    return function(){
+
+    }
+})
