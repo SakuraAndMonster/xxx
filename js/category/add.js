ca2b4d881b193c7be71471cba798c5f2e3717cc7
sun <sun@qq.com>
2017-10-23 15:08:15 +0800

    修改

--- js/category/add.js
+++ js/category/add.js
@@ -22,9 +22,32 @@ define([
                 //把数据编译到模板中
                 var categoryAdd=art.render(categoryAddTpl,res);
                 //把编译成功之后的内容放到页面中
+                var $categoryAdd=$(categoryAdd)
+                    .on("submit","form",function(e){
+                        e.preventDefault();
+
+                        //获取表单数据
+                        var formData=$(this).serialize();
+
+                        //发送ajax请求
+                        $.ajax({
+                            url:"/api/category/add",
+                            type:"post",
+                            data:"formData",
+                            success:function(res){
+                                if(res.code!=200) throw new Error(res.msg);
+
+                                //关闭模态框-->隐藏模态框
+                                $categoryAdd.modal("hide");
+                                //模拟点击课程分类按钮-->从而实现刷新分类列表
+                                $(".list-group a[v=category]").trigger("click");
+                            }
+                        })
+                    })
+                    .myModal();
             }
         })
-  
+
         var $categoryAdd=$(categoryAddTpl).myModal();
 
 
